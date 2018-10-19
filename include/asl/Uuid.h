b1b76921164d549f0464d7ea066a2321659ab595
Alvaro <alvaro.segura@gmail.com>
2018-10-19 23:30:14 +0200

    Added UUID generator

---
+++ include/asl/Uuid.h
@@ -0,0 +1,68 @@
+// Copyright(c) 1999-2018 ASL author
+// Licensed under the MIT License (http://opensource.org/licenses/MIT)
+
+#ifndef ASL_UUID_H
+#define ASL_UUID_H
+
+#include <asl/String.h>
+
+namespace asl {
+
+/**
+Represents an UUID which can be generated to be unique.
+
+~~~
+Uuid u = Uuid::generate();   // this is a binary ID (16 bytes)
+String s = u;                // convert to a string representation
+~~~
+
+Or directly:
+
+~~~
+String u = Uuid::generate();
+~~~
+*/
+class ASL_API Uuid
+{
+public:
+	Uuid() {}
+	/**
+	Constructs an UUID from a string representation
+	*/
+	Uuid(const String& s);
+	/**
+	Returns a string representation of this UUID
+	*/
+	String operator*() const;
+	operator String() const { return **this; }
+	/**
+	Returns the i-th byte of the identifier (out of 16)
+	*/
+	byte& operator[](int i) { return _u[i]; }
+	byte operator[](int i) const { return _u[i]; }
+	bool operator==(const Uuid& u) const { return memcmp(_u, u._u, 16) == 0; }
+	bool operator<(const Uuid& u) const { return memcmp(_u, u._u, 16) < 0; };
+	bool operator!=(const Uuid& u) const { return !(*this == u); }
+	/**
+	Generates an UUID (version 4).
+	*/
+	static Uuid generate();
+
+private:
+	byte _u[16];
+};
+
+/*
+An UUID generator, currently generates UUIDs in the standard string representation.
+*/
+class ASL_API UuidGenerator
+{
+public:
+	UuidGenerator();
+	Uuid generate();
+private:
+	Random _random;
+};
+
+}
+#endif
---
+++ src/Uuid.cpp
@@ -0,0 +1,49 @@
+#include <asl/Uuid.h>
+
+namespace asl {
+
+Uuid::Uuid(const String& s)
+{
+	if (s.length() != 36)
+		return;
+	for (int i = 0, j = 0; i < 16; i++, j+=2)
+	{
+		_u[i] = (byte)s.substr(j, 2).hexToInt();
+		if (i == 3 || i == 5 || i == 7 || i == 9)
+			j++;
+	}
+}
+
+String Uuid::operator*() const
+{
+	return String(36, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+		_u[0], _u[1], _u[2], _u[3], _u[4], _u[5], _u[6], _u[7], _u[8], _u[9], _u[10], _u[11], _u[12], _u[13], _u[14], _u[15]);
+}
+
+UuidGenerator::UuidGenerator()
+{
+	_random.init();
+}
+
+Uuid UuidGenerator::generate()
+{
+	Uuid u;
+	_random.get();
+	for(int i=0; i<16; i++)
+	{
+		int r = _random.get();
+		r >>= (r & 0xf);
+		u[i] = (byte)(r & 0xff);
+	}
+	u[6] = (u[6] & ~0xf0) | 0x40; // version 4
+	u[8] = (u[8] & ~0xc0) | 0x80; // variant 1
+	return u;
+}
+
+Uuid Uuid::generate()
+{
+	static UuidGenerator gen;
+	return gen.generate();
+}
+
+}
