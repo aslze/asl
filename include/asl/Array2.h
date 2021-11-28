ea3431033858911923daa06adf4997504c78cde1
Alvaro <alvaro.segura@gmail.com>
2021-11-28 02:00:24 +0100

    Improved Array2 (initializers, slice, with, enumerators...)

--- include/asl/Array2.h
+++ include/asl/Array2.h
@@ -8,13 +8,27 @@
 
 namespace asl {
 
+struct IndexIJEnumerator;
+
 /**
-A simple 2-dimensional array or matrix
+A simple 2-dimensional dynamic array or matrix
+
+~~~
+Array2<int> a(3, 3);
+
+Array2<float> b = {
+	{ 1, 2, 3 },
+	{ 3, -1.5f, 0 }
+};
+
+b(1, 1) = 7.25f;
+~~~
+
 */
 template <class T>
 class Array2
 {
-	asl::Array<T> _a;
+	Array<T> _a;
 	int _rows, _cols;
 public:
 
@@ -38,6 +52,61 @@ public:
 	{}
 
 	/**
+	Creates an array of rows x cols elements and copies them from the pointer p (row-wise)
+	*/
+	Array2(int rows, int cols, const T* p) :
+		_a(rows * cols, p), _rows(rows), _cols(cols)
+	{}
+
+	Array2(int rows, int cols, const Array<T>& a) :
+		_a(a), _rows(rows), _cols(cols)
+	{}
+
+#ifdef ASL_HAVE_INITLIST
+	/**
+	Creates an array with size rows x cols and the given elements
+	*/
+	Array2(int rows, int cols, std::initializer_list<T> a) :
+		_a(a), _rows(rows), _cols(cols)
+	{}
+
+	/**
+	Creates an array with given list of lists of elements
+	*/
+	Array2(std::initializer_list<std::initializer_list<T> > a)
+	{
+		_rows = (int)a.size();
+		if (_rows == 0)
+			return;
+		const std::initializer_list<T>* p = a.begin();
+		_cols = (int)p->size();
+		_a.resize(_rows * _cols);
+		for (int i = 0; i < _rows; i++)
+		{
+			const T* q = p[i].begin();
+			for (int j = 0; j < _cols; j++)
+				(*this)(i, j) = q[j];
+		}
+	}
+#endif
+
+	/**
+	Creates a copy of this array with items converted to type K
+	*/
+	template<class K>
+	Array2<K> with() const
+	{
+		return Array2<K>(_rows, _cols, _a.template with<K>());
+	}
+
+	Array<T>& data() { return _a; }
+
+	/**
+	Returns the internal array holding all values (row-major)
+	*/
+	const Array<T>& data() const { return _a; }
+
+	/**
 	Returns an independent copy of this array
 	*/
 	Array2 clone() const
@@ -50,7 +119,19 @@ public:
 	}
 
 	/**
-	Resizes the matrix to the r x c
+	Returns a sub-array consisting of a rows [i1, i2) and columns [j1, j2)
+	*/
+	Array2 slice(int i1, int i2, int j1, int j2) const
+	{
+		Array2 b(i2-i1, j2-j1);
+		for (int i = 0; i < b.rows(); i++)
+			for (int j = 0; j < b.cols(); j++)
+				b(i, j) = (*this)(i1 + i, j1 + j);
+		return b;
+	}
+
+	/**
+	Resizes the matrix to r x c
 	*/
 	void resize(int r, int c)
 	{
@@ -86,12 +167,56 @@ public:
 		return _a[i * _cols + j];
 	}
 
+	/**
+	Sets all array items to value x
+	*/
 	void set(const T& x)
 	{
 		for (int i = 0; i < _a.length(); i++)
 			_a[i] = x;
 	}
+
+	IndexIJEnumerator indices() const;
 };
+
+struct IndexIJ { int i, j; };
+
+struct IndexIJEnumerator
+{
+	IndexIJ i;
+	int m, n;
+	bool more;
+	IndexIJEnumerator all() const { return *(IndexIJEnumerator*)this; }
+	IndexIJEnumerator(int m, int n) : m(m), n(n) { i.i = 0; i.j = 0; more = m * n > 0; }
+	void operator++() { if (++i.j >= n) { i.i++; i.j = 0; if (i.i == m) more = false; } }
+	operator bool() const { return more; }
+	//void operator++() { if (++i.j >= n) { i.i++; i.j = 0; }	}
+	//operator bool() const { return i.j < n && i.i < m; }
+	bool operator!=(const IndexIJEnumerator& e) const { return (bool)*this; }
+	const IndexIJ& operator*() const { return i; }
+};
+
+template <class T>
+inline IndexIJEnumerator Array2<T>::indices() const { return IndexIJEnumerator(rows(), cols()); }
+
+#ifdef ASL_HAVE_RANGEFOR
+
+inline IndexIJEnumerator begin(const IndexIJEnumerator& e) { return e.all(); }
+inline IndexIJEnumerator end(const IndexIJEnumerator& e) { return e.all(); }
+
+template<class T>
+typename Array<T>::Enumerator begin(const Array2<T>& a)
+{
+	return a.data().all();
+}
+
+template<class T>
+typename Array<T>::Enumerator end(const Array2<T>& a)
+{
+	return a.data().all();
+}
+
+#endif
 
 }
 
