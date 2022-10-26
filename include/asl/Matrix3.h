0cbd08164b78673f24c6cb75c7693c94583ef19a
Alvaro <alvaro.segura@gmail.com>
2022-10-26 23:37:25 +0200

    Added Matrix3::norm()

--- include/asl/Matrix3.h
+++ include/asl/Matrix3.h
@@ -243,6 +243,24 @@ public:
 	{
 		return a[0][0] * (a[1][1] * a[2][2] - a[2][1] * a[1][2]) + a[1][0] * (a[2][1] * a[0][2] - a[0][1] * a[2][2]) + a[2][0] * (a[0][1] * a[1][2] - a[1][1] * a[0][2]);
 	}
+
+	/**
+	Returns the matrix Frobenius norm squared
+	*/
+	T normSq() const
+	{
+		T t = 0;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				t += sqr(a[i][j]);
+		return t;
+	}
+
+	/**
+	Returns the matrix Frobenius norm
+	*/
+	T norm() const { return sqrt(normSq()); }
+
 private:
 	T a[3][3];
 };
