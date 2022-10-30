a7d429a3d44d12c2c294627dbf42113dbc72e3e6
Alvaro <alvaro.segura@gmail.com>
2022-10-30 20:08:28 +0100

    Improved CmdArgs: can get free arguments before options, can get option values like --, etc.

--- include/asl/CmdArgs.h
+++ include/asl/CmdArgs.h
@@ -15,7 +15,7 @@ It allows to read named options (starting with a `-`), optionally followed by a 
 arguments after the last option. Options without value are flags and should end with a '!'.
 
 If an option is given more than once its value is the last one, but all its values are accessible with the () operator.
-The arguments given by this object with the `length()` property and the `[int]` operator are the rest arguments, not
+The arguments given by this object with the `length()` property and the `[int]` operator are the free arguments, not
 counting options or the program name. The whole array of arguments received at `main()` can be obtained with the `all()`
 function.
 
@@ -25,7 +25,7 @@ For example:
 convert -format jpeg -fast! -q 85 image1.png image2.bmp
 ~~~
 
-Has the option `format` with value `jpeg`, the flag `fast`, the option `q` with value 85 and 2 rest arguments.
+Has the option `format` with value `jpeg`, the flag `fast`, the option `q` with value 85 and 2 free arguments.
 
 That can be read with:
 
@@ -33,7 +33,7 @@ That can be read with:
 CmdArgs args(argc, argv);
 
 String format = args["format"];
-int quality = args("q", 90);             // use value 90 if the option is not given
+int quality = args["q"] | 90;             // use value 90 if the option is not given
 bool fast = args.is("fast");
 
 for( int i=0; i < args.length(); i++)
@@ -82,15 +82,17 @@ String name = args["name"];
 class ASL_API CmdArgs
 {
 public:
+#if defined _WIN32 || defined __linux__
 	/**
 	Constructs a CmdArgs object with the arguments of the current process (only on Windows and Linux).
 	*/
 	CmdArgs(const String& spec = "");
+#endif
 	/** Constructs a CmdArgs object from the arguments to main() and an optional specification*/
-	CmdArgs(int argc, char* argv[], const String& spec = "");
+	CmdArgs(int argc, char** argv, const String& spec = "") { parse(argc, argv, spec); }
 	/** Tests if the given option exists */
 	bool has(const String& opt) const;
-	/** Returns the value of a flag option (if it exists with no value or has a value 1, true, yes or on) */
+	/** Returns the value of a flag option (if it exists with no value or has a value 1, true or yes) */
 	bool is(const String& opt) const;
 	/** Returns the value of option named `opt` or an empty string of it was not defined */
 	String operator[](const String& opt) const;
@@ -100,15 +102,17 @@ public:
 	String operator[](int i) const;
 	/** Returns an array with all values of option `opt` if more than one was given */
 	Array<String> operator()(const String& opt) const;
-	/** Returns the rest of arguments after the last option */
+	/** Returns the free arguments, arguments excluding options */
 	Array<String> rest() const;
+	Dic<String> options() const { return _opts; }
 	/** Returns all the arguments, as received at main() */
 	Array<String> all() const;
-	/** Returns the number of rest arguments, those after the options */
+	/** Returns the number of free arguments, those excluding options */
 	int length() const;
 	/** Returns the options that have not been tested (probably wrong options) */
 	const Array<String> untested() const { return _unused; }
 private:
+	void parse(int argc, char** argv, const String& spec = "");
 	void use(const String& opt) const;
 	Array<String> _args;
 	Array<String> _rest;
